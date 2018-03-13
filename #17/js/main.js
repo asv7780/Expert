310d5bc0735e638e26d0295bdfc0289b31a5ed29
asv7780 <asv7780@gmail.com>
2018-03-13 12:02:52 +0200

    HW #17

--- #17/js/main.js
+++ #17/js/main.js
@@ -5,22 +5,22 @@ function transform() {
     ваша программа
     по возможности разбейте на функции
     */
-    /*1. С помощью функции splice необходимо вырезать 6-й элемент массива. В результате ваш массив должен стать короче на один элемент.*/
-
-    data.splice(5, 1);
-
-    /*2. Используйте функцию forEach. Внутри цикла создайте новый массив объектов. В процессе создания нового массива объектов, избавьтесь от ключа id.То есть в вашем новом массиве не должно быть id в каждом конкретном объекте.*/
-
+    /*1. С помощью функции splice необходимо вырезать 6-й элемент массива. В результате ваш массив должен стать короче на один элемент.
+    2. Используйте функцию forEach. Внутри цикла создайте новый массив объектов. В процессе создания нового массива объектов, избавьтесь от ключа id.То есть в вашем новом массиве не должно быть id в каждом конкретном объекте.*/
     let newArr = [];
-    data.forEach(function (item) {
-        newArr.push({
-            url: item.url,
-            name: item.name,
-            params: item.params,
-            description: item.description,
-            date: item.date
-        })
-    });
+
+    (function makeNewArr() {
+        data.splice(5, 1);
+        data.forEach(function (item) {
+            newArr.push({
+                url: item.url,
+                name: item.name,
+                params: item.params,
+                description: item.description,
+                date: item.date
+            })
+        });
+    }());
 
     /*3 По новому массиву объектов, полученному с помощью функции forEach пройдитесь методом map()
      4 На каждой итерации цикла мы получаем один объект из массива объектов. Берем этот объект и преобразоваем его поля по следующим правилам.
@@ -32,49 +32,64 @@ function transform() {
  То есть params.status и params.progress
  11. Создать новое поле isVisible. Переложить в это поле значение поля params.status.*/
 
-    let mapedArr = newArr.map(item => {
-        return {
-            url: `http://${item.url}`,
-            name: newName(item.name),
-            params: `${item.params.status}=>${item.params.progress}`,
-            isVisible: item.params.status,
-            description: newDescription(item.description),
-            date: moment(item.date).format('YYYY/MM/DD, HH:mm')
-        };
-
-        function newName(name) {
-            name = name.toLowerCase();
-            return `${name.substr(0, 1)/*or name[0]*/.toUpperCase()}${name.substr(1)}`;
+    function newUrl(url) {
+        if (url.indexOf('http://') !== 0) {
+            return `http://${url}`;
         }
+        return url;
+    }
 
-        function newDescription(description) {
-            let longDescription = description.substring(16);
-            return (description.length >= 15) ? description.replace(longDescription, "...") : description;
+    function newName(name) {
+        name = name.toLowerCase();
+        return `${name[0].toUpperCase()}${name.substr(1)}`;
+    }
 
-        }
-    });
+    function newParam(params) {
+        return `${params.status}=>${params.progress}`;
+    }
+
+    function newDescription(description) {
+        return (description.length >= 15) ? `${description.substring(0, 15)}...` : description;
+    }
+
+    function newDate(date) {
+        return moment(date).format('YYYY/MM/DD, HH:mm');
+    }
+
+    function mapedArr() {
+        newArr.map(item => {
+            return {
+                url: newUrl(item.url),
+                name: newName(item.name),
+                params: newParam(item.params),
+                isVisible: item.params.status,
+                description: newDescription(item.description),
+                date: newDate(item.date)
+            };
+        });
+    }
 
     /*12. После всех преобразований функция map вернет вам новый массив. Теперь с помощью функции filter вам необходимо выбрать только те элементы у которых isVisible == true. Пример работы функции filter есть в презентации.*/
 
-    let result = mapedArr.filter(item => item.isVisible === true);
-    /*or (item => item.isVisible) */
+    function getFilteredArr(newArr) {
+        return newArr.filter(item => item.isVisible);
+    }
 
     /*13. Полученный результат печатаем в консоль.
     Для печати используем отдельную функцию как в прошлых заданиях. То есть вынесете console.log в отдельную функцию.*/
 
-    function printResult(array) {
-        console.log(array);
+    function printResult(result) {
+        console.log(result);
     }
 
-    printResult(result);
+    printResult(getFilteredArr(mapedArr(newArr)));
 
 
     /*14. Пример результата (количество элементов в результате должно быть не два а сколько укажете в переменной):
     */
 
-    btn.addEventListener("click", transform)
 }
 
-transform();
+btn.addEventListener("click", transform);
 
 
